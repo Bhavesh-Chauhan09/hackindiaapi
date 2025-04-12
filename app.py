40236d0fc6d0158f1f43aa46b23f85c22c7d62d0
Bhavesh-Chauhan09 <130345635+Bhavesh-Chauhan09@users.noreply.github.com>
2025-04-12 09:01:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import os
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def home():
     return "Flask TF-IDF Keyword Extractor is running!"
